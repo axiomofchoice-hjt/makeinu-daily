bebf66b66bf2fb0b0200b938eec7d3b74e9bd51a
axiomofchoice-hjt <Axiomofchoice@163.com>
2026-07-26 14:49:06 +0800

    Refactor daily report generation to use current date with optional delta argument

--- script/new_daily.py
+++ script/new_daily.py
@@ -1,48 +1,40 @@
 from datetime import datetime, timedelta
 from articles import get_articles, path_to_date_str, src_path
 from markdown_file import read_markdown_file, write_markdown_file
-
-articles = [path_to_date_str(path) for path in get_articles()["daily"]]
-
-previous = max(articles)
-previous_ = datetime.strptime(previous, r"%Y/%m/%d")
-current_ = previous_ + timedelta(days=1)
-current = current_.strftime(r"%Y/%m/%d")
-
-previous_file = src_path() / f"{previous}.md"
-current_file = src_path() / f"{current}.md"
+from argparse import ArgumentParser
 
 
-# 更新 <root>/<previous>.md
-header, content = read_markdown_file(previous_file)
-header["next"] = {
-    "link": "/" + current,
-    "text": "败犬日报 " + current.replace("/", "-"),
-}
-write_markdown_file(previous_file, header, content)
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("delta", nargs='?', type=int, default=0)
+    return parser.parse_args()
 
-# 创建 <root>/<current>.md
-header = {
-    "date": current,
-    "title": "败犬日报 " + current.replace("/", "-"),
-    "next": False,
-    "prev": {
-        "link": "/" + previous,
-        "text": "败犬日报 " + previous.replace("/", "-"),
-    },
-}
-current_file.parent.mkdir(parents=True, exist_ok=True)
-write_markdown_file(
-    current_file,
-    header,
-    f"\n# 败犬日报 {current.replace("/", "-")}\n\n[[toc]]\n\n今日无话题收录 :kissing_heart:\n",
-)
 
-print(current_file)
+if __name__ == "__main__":
+    args = parse_args()
+    current_ = datetime.now() + timedelta(days=args.delta)
+    current = current_.strftime(r"%Y/%m/%d")
+    current_file = src_path() / f"{current}.md"
 
-# 更新 <root>/index.md
-index_file = src_path() / "index.md"
-header, content = read_markdown_file(index_file)
-header["hero"]["actions"][0]["link"] = current
-header["features"][0]["link"] = current
-write_markdown_file(index_file, header, content)
+    # 创建 <root>/<current>.md
+    header = {
+        "date": current,
+        "title": "败犬日报 " + current.replace("/", "-"),
+        "next": False,
+        "prev": False,
+    }
+    current_file.parent.mkdir(parents=True, exist_ok=True)
+    write_markdown_file(
+        current_file,
+        header,
+        f"\n# 败犬日报 {current.replace("/", "-")}\n\n[[toc]]\n\n今日无话题收录 :kissing_heart:\n",
+    )
+
+    print(current_file)
+
+    # 更新 <root>/index.md
+    index_file = src_path() / "index.md"
+    header, content = read_markdown_file(index_file)
+    header["hero"]["actions"][0]["link"] = current
+    header["features"][0]["link"] = current
+    write_markdown_file(index_file, header, content)
