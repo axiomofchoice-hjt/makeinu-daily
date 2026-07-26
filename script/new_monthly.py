9a535add4da25d38c612ab5cbe4c58f7a43baee3
axiomofchoice-hjt <Axiomofchoice@163.com>
2026-07-26 14:52:10 +0800

    Refactor new monthly report generation to use command-line argument for delta days

--- script/new_monthly.py
+++ script/new_monthly.py
@@ -1,42 +1,36 @@
 from datetime import datetime, timedelta
 from articles import get_articles, path_to_date_str, src_path
 from markdown_file import read_markdown_file, write_markdown_file
-
-articles = [path_to_date_str(path) for path in get_articles()["monthly"]]
-
-previous = max(articles)
-previous_ = datetime.strptime(previous, r"%Y/%m")
-current_ = previous_ + timedelta(days=31)
-current = current_.strftime(r"%Y/%m")
-next_ = current_ + timedelta(days=31)
-next = next_.strftime(r"%Y/%m")
-
-previous_file = src_path() / previous / "index.md"
-current_file = src_path() / current / "index.md"
+from argparse import ArgumentParser
 
 
-# 更新 <root>/<previous>/index.md
-header, content = read_markdown_file(previous_file)
-header["next"] = {
-    "link": "/" + current,
-    "text": "败犬のC++每月精选 " + current.replace("/", "-"),
-}
-write_markdown_file(previous_file, header, content)
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("delta", nargs="?", type=int, default=0)
+    return parser.parse_args()
 
-# 创建 <root>/<today>.md
-header = {
-    "date": next + "/01",
-    "title": "败犬のC++每月精选 " + current.replace("/", "-"),
-    "next": False,
-    "prev": {
-        "link": "/" + previous,
-        "text": "败犬のC++每月精选 " + previous.replace("/", "-"),
-    },
-}
-write_markdown_file(
-    current_file,
-    header,
-    f"\n# 败犬のC++每月精选 {current.replace("/", "-")}\n\n[[toc]]\n",
-)
 
-print(current_file)
+if __name__ == "__main__":
+    args = parse_args()
+    current_ = datetime.now()
+    current = current_.strftime(r"%Y/%m")
+    current_ = datetime.strptime(current, r"%Y/%m") + timedelta(days=args.delta * 31)
+    current = current_.strftime(r"%Y/%m")
+    current_file = src_path() / current / "index.md"
+    next_ = current_ + timedelta(days=31)
+    next = next_.strftime(r"%Y/%m")
+
+    # 创建 <root>/<today>.md
+    header = {
+        "date": next + "/01",
+        "title": "败犬のC++每月精选 " + current.replace("/", "-"),
+        "next": False,
+        "prev": False,
+    }
+    write_markdown_file(
+        current_file,
+        header,
+        f"\n# 败犬のC++每月精选 {current.replace("/", "-")}\n\n[[toc]]\n",
+    )
+
+    print(current_file)
